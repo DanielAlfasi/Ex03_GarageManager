9822af8e9f2bf6148bf060ee65923b311df15a4f
Gal Botzer <Galbotzer136@gmail.com>
2023-05-29 20:11:09 +0300

    new changes

---
+++ Ex03.GarageLogic/VehicleGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    public class VehicleGenerator
+    {
+        public static Vehicle GenerateNewVehicle(string i_ModelName, string i_LicensePlate)
+        {
+            return null;
+        } 
+
+        private static Wheel[] generateWheel()
+        {
+            return null;
+        }
+    }
+}
